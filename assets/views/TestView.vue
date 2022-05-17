902387c6adec72879713627fa6cc1ccb3fe1d4be
Jonathan-Caillon <j.caillon@codeur.online>
2022-05-17 13:50:48 +0200

    init

---
+++ assets/views/TestView.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>TEST</main>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    });
+    let myInit = {
+      method: "GET",
+      headers: headers,
+      mode: "cors",
+      cache: "default",
+    };
+    const response = await fetch("/api/users", myInit);
+    const data = await response.json();
+    this.obj = data;
+    console.log(data);
+  },
+};
+</script>
