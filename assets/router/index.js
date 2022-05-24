1874aa0cdf6ea37351791a0811540089f445ecc2
Jonathan-Caillon <j.caillon@codeur.online>
2022-05-24 11:18:29 +0200

    login and sass

--- assets/router/index.js
+++ assets/router/index.js
@@ -26,6 +26,14 @@ const router = createRouter({
       component: () => import("../views/TestView.vue"),
     },
     {
+      path: "/login",
+      name: "login",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/LoginView.vue"),
+    },
+    {
       path: "/:catchAll(.*)",
       name: "404",
       // route level code-splitting
