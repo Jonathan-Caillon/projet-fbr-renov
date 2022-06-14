b297dafc58aa79c328d01b795c541bd1ae76e1e6
Jonathan-Caillon <j.caillon@codeur.online>
2022-06-14 15:22:05 +0200

    app

--- assets/app.js
+++ assets/app.js
@@ -6,31 +6,19 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import "./styles/app.css";
 
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";
 
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-<<<<<<< HEAD
 //SWEETALERT
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-=======
-//Sweetalert2
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-
->>>>>>> f8f35d4a88fb644765b053d483091107b6d1b7c2
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 //ICONE
-import '@fortawesome/fontawesome-free/js/all'
+import "@fortawesome/fontawesome-free/js/all";
 
-
-<<<<<<< HEAD
-createApp(App).use(router).use(VueSweetalert2).mount('#app');
-=======
-createApp(App).use(router).use(VueSweetalert2).mount('#app');
->>>>>>> f8f35d4a88fb644765b053d483091107b6d1b7c2
+createApp(App).use(router).use(VueSweetalert2).mount("#app");
